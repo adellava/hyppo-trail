9c237ab02ed51ee7913dfb5beb19d468eb09411d
Antonio Dell'Ava <antonio.dellava@edreamsodigeo.com>
2023-12-20 14:49:59 +0100

    .next removed

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/antonio.dellava/hyppo-trail/src/app/layout\":[\"static/media/3d9ea938b6afa941-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
