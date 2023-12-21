d4d7c12deb77dffd8371163d77d5342999bb2bae
Antonio Dell'Ava <antonio.dellava@edreamsodigeo.com>
2023-12-21 06:37:22 +0100

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,43 +33,56 @@ export default function Home() {
         </a>
         <CoverImage />
       </section>
-      <section>
-        <h2 className={styles.subtitle}>Trail</h2>
-        <p>
-          La Hippo Bike Trail si sviluppa su un percorso ad anello, con partenza
-          e arrivo a Habitat le Bandie, lungo 210 km ( sterrato all’85% ) con
-          4500 metri di dislivello. Dentro la Hyppo trail troverete tutti gli
-          ingredienti di un vero trail: hike-a-bike, guadi, un lungo tratto in
-          quota senza presenza umana, discese scassate, ... insomma non
-          presentatevi se non avete almeno una gomma da 45mm 😜
-          <b>
-            La sfida è concluderlo entro 18 ore (ore 1:00 del 14 Aprile).
-          </b>{" "}
-          Se lo concludete con successo potete ritenervi pronti per i trail più
-          lunghi.
-        </p>
-        <p>
-          Se vuoi sapere di più leggi il{" "}
-          <a
-            target="_blanck"
-            href="https://docs.google.com/document/d/1tFjGzyMs6XhUHhFCs37sNum7AuV8YPR8qFyQjkTRTF4/view"
-          >
-            regolamento dell’evento
-          </a>
-        </p>
-        <Map />
+      <section className={styles.row}>
+        <div className={styles.row__column}>
+          <h2 className={styles.subtitle}>Trail</h2>
+          <p className={styles.paragraph}>
+            La Hippo Bike Trail si sviluppa su un percorso ad anello, con
+            partenza e arrivo a Habitat le Bandie, lungo 210 km ( sterrato
+            all’85% ) con 4500 metri di dislivello. Dentro la Hyppo trail
+            troverete tutti gli ingredienti di un vero trail: hike-a-bike,
+            guadi, un lungo tratto in quota senza presenza umana, discese
+            scassate, ... insomma non presentatevi se non avete almeno una gomma
+            da 45mm 😜
+            <b>
+              La sfida è concluderlo entro 18 ore (ore 1:00 del 14 Aprile).
+            </b>{" "}
+            Se lo concludete con successo potete ritenervi pronti per i trail
+            più lunghi.
+          </p>
+          <p className={styles.paragraph}>
+            Se vuoi sapere di più leggi il{" "}
+            <a
+              target="_blanck"
+              href="https://docs.google.com/document/d/1tFjGzyMs6XhUHhFCs37sNum7AuV8YPR8qFyQjkTRTF4/view"
+            >
+              regolamento dell’evento
+            </a>
+          </p>
+        </div>
+        <div className={styles.row__column}>
+          <Map />
+        </div>
       </section>
-      <section>
-        <h2 className={styles.subtitle}>Partner</h2>
-        <MorettiLogo />
-        <HabitatLogo />
-        <h2 className={styles.subtitle}>Chi Siamo</h2>
-        <AmgLogo />
-        <p>
-          Il trail è organizzato da AMG Alta Marca Gravel. Siamo nati con lo
-          scopo di creare momenti di condivisione per tutti gli appassionati del
-          ciclismo. Questo è uno di quei momenti.
-        </p>
+      <section className={styles.row}>
+        <div className={styles.row__column}>
+          <h2 className={styles.subtitle}>Partner</h2>
+          <div className={styles.horizonatalFlex}>
+            <MorettiLogo />
+            <HabitatLogo />
+          </div>
+        </div>
+        <div className={styles.row__column}>
+          <h2 className={styles.subtitle}>Chi Siamo</h2>
+          <div className={styles.horizonatalFlex}>
+            <AmgLogo />
+            <p className={styles.paragraph}>
+              Il trail è organizzato da AMG Alta Marca Gravel. Siamo nati con lo
+              scopo di creare momenti di condivisione per tutti gli appassionati
+              del ciclismo. Questo è uno di quei momenti.
+            </p>
+          </div>
+        </div>
       </section>
 
       <footer className={styles.footer}>
